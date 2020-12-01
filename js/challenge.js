af00755bb987fbd193262965738dbc8fd3ff72c6
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 21:43:11 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,21 +1,21 @@
-let number = document.getElementById("counter")
-let plus = document.getElementById("plus")
-let minus = document.getElementById("minus")
-let pause = document.getElementById("pause")
+// let number = document.getElementById("counter")
+// let plus = document.getElementById("plus")
+// let minus = document.getElementById("minus")
+// let pause = document.getElementById("pause")
 
-let timer = number.addEventListener("DOMContentLoaded", setInterval(upNumber, 1000));
+// let timer = number.addEventListener("DOMContentLoaded", setInterval(upNumber, 1000));
 
 
-function upNumber(){
-  number.textContent = parseInt(number.textContent) + 1
-}
+// function upNumber(){
+//   number.textContent = parseInt(number.textContent) + 1
+// }
 
-function downNumber(){
-  number.textContent = parseInt(number.textContent) - 1
-}
+// function downNumber(){
+//   number.textContent = parseInt(number.textContent) - 1
+// }
 
-plus.addEventListener("click", upNumber);
-minus.addEventListener("click", downNumber);
-pause.addEventListener("click", function(){
-  clearTimeout(timer);
-})
+// plus.addEventListener("click", upNumber);
+// minus.addEventListener("click", downNumber);
+// pause.addEventListener("click", function(){
+//   clearTimeout(timer);
+// })
