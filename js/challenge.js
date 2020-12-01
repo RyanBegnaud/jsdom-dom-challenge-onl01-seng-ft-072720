d1be9c00c1be886074e31e1ade8af78a942e7a4f
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 21:13:58 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,5 +1,5 @@
 let number = document.getElementById("counter")
 number.addEventListener("DOMContentLoaded", function(e){
   let num = parseInt(number.innerHTML)
-  setInterval(function(){number.innerHTML += 1 }, 1000);
+  setInterval(function(){num += 1 }, 1000);
 })
