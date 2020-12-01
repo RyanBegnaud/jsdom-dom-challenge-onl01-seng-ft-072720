a0aa74825d13cdb9dd33685f259e223c7fc66bd0
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 21:28:31 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -2,7 +2,7 @@ let number = document.getElementById("counter")
 let plus = document.getElementById("plus")
 
 number.addEventListener("DOMContentLoaded", setInterval(upNumber, 1000));
-plus.addEventListener("click", upNumber);
+plus.addEventListener("click", upNumber());
 
 function upNumber(){
   number.textContent = parseInt(number.textContent) + 1
