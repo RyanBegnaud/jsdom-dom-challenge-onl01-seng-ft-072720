4526f62d07e16cda0a180b4c0978c286c25684f8
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 21:43:14 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -19,3 +19,35 @@
 // pause.addEventListener("click", function(){
 //   clearTimeout(timer);
 // })
+
+
+let counter_id = document.getElementById("counter")
+let pause_id = document.getElementById("pause")
+let minus_id = document.getElementById("-")
+let plus_id = document.getElementById("+")
+let heart_id = document.getElementById("<3")
+let list_id = document.getElementById("list")
+let comment_id = document.getElementsByTagName("form")[0]
+
+let life = true;
+
+let counter = 0
+//As a user, i should see the timer increment every second once the page has loaded
+let timer = setInterval(function(){
+  if (life){
+    counter_id.innerHTML = counter
+    counter += 1;
+  }
+
+}, 1000);
+
+//As a user, i can manually increment and decrement the counter as i like
+plus_id.addEventListener("click", function(){
+  counter += 1;
+  counter_id.innerHTML = parseInt(counter_id.innerHTML) + 1
+})
+
+minus_id.addEventListener("click", function(){
+  counter -= 1;
+  counter_id.innerHTML = parseInt(counter_id.innerHTML) - 1
+})
